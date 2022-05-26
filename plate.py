fef94f088959476e2196268c91a485e2cb23e945
kalo10 <105011388+kalo10@users.noreply.github.com>
2022-05-26 11:48:19 +0530

    Add files via upload

---
+++ plate.py
@@ -0,0 +1,33 @@
+def alter_stack(command, n):
+    ID = ""
+    top = 0
+    stack = [0]*100
+    for i in range(n):
+        ID = ""
+        if command[i][0] == 'A':
+            top += 1
+            for j in range(9, len(command[i])):
+                ID = ID + command[i][j]
+            ID = int(ID)
+            stack[top] = ID
+            
+        elif command[i] == "Check":
+            if top <= 0:
+                print("Invalid")
+                top = 0
+            else: 
+                print(stack[top])
+                
+        
+        elif command[i] == "PickPlate":
+            top -= 1
+            if top < 0:
+                print("Invalid")
+                top = 0
+
+n = int(input())
+command = [0]*100
+top = 0
+for i in range(n):
+    command[i] = input()
+alter_stack(command, n)
